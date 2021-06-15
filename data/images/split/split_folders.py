d63259f337569b3da297f0960b27edd6aaf0072e
csbroadbent <broad123@umn.edu>
2021-06-15 10:48:46 -0500

    split images

--- data/images/split/split_folders.py
+++ data/images/split/split_folders.py
@@ -1,3 +1,3 @@
 import splitfolders
 
-splitfolders.ratio('cropped_length', output='train_val_test_crop', seed=0, ratio=(0.70, 0.15, 0.15))
+splitfolders.ratio('aug', output='train_val_test_crop', seed=0, ratio=(0.70, 0.15, 0.15))
